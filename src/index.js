c26348158e53cf299d6592d8b84d851dba4a5e1c
Jaidyn Holt <116743846+jaidynholt@users.noreply.github.com>
2025-09-13 23:11:35 -0400

    Modify portfolio filters to be sections

--- src/index.js
+++ src/index.js
@@ -46,54 +46,61 @@ function ScrollToPosition(pixelsFromTop) {
         behavior: 'smooth'
     });
 }
-
-function ToggleFilter(filterId) {
-    // get the filter button that called this
-    const filterButton = document.getElementById(filterId);
-
-    // activate 'all'
-    if (filterId == 'all' && !filterButton.classList.contains('filterOn')){
-        // deactivate everything else first
-        const filterButtons = document.getElementsByClassName('filterButton');
-        for (var i = 0; i < filterButtons.length; i++){ filterButtons[i].classList.remove('filterOn'); }
-
-        // then activate all
-        filterButton.classList.add('filterOn');
-        FilterProjects(filterId, true);
-    }
-    // activating something other than 'all'
-    else if (!filterButton.classList.contains('filterOn')) {
-        // if 'all' is activated, deactivate it and deactivate all projects
-        if (document.getElementById('all').classList.contains('filterOn')){
-            document.getElementById('all').classList.remove('filterOn');
-            FilterProjects('all', false);
-        }
-
-        // then activate the filter
-        filterButton.classList.add('filterOn');
-        FilterProjects(filterId, true);
-    }
-    // deactivate something other than 'all'
-    else if (filterId != 'all' && filterButton.classList.contains('filterOn')){
-        filterButton.classList.remove('filterOn');
-        FilterProjects(filterId, false);
-    }
+function ScrollToSection(id) {
+    const section = document.getElementById(id);
+    section.scrollIntoView({
+        block: 'start',
+        behavior: 'smooth'
+    });
 }
 
-function FilterProjects(selectedFilter, isOn) {
-    if (selectedFilter == 'all'){
-        // get all the projects
-        const projects = document.getElementsByClassName('projectBox');
-        // make all visible
-        if (isOn) { for (var i = 0; i < projects.length; i++){ projects[i].classList.add('filteredOn'); } }
-        else { for (var i = 0; i < projects.length; i++){ projects[i].classList.remove('filteredOn'); } }
-    }
-    else {
-        // get all the projects with the filter name
-        const projects = document.getElementsByClassName(selectedFilter);
-        if (isOn) { for (var i = 0; i < projects.length; i++){ projects[i].classList.add('filteredOn'); } }
-        else { for (var i = 0; i < projects.length; i++){ projects[i].classList.remove('filteredOn'); } }
-    }
-}
+// function ToggleFilter(filterId) {
+//     // get the filter button that called this
+//     const filterButton = document.getElementById(filterId);
+
+//     // activate 'all'
+//     if (filterId == 'all' && !filterButton.classList.contains('filterOn')){
+//         // deactivate everything else first
+//         const filterButtons = document.getElementsByClassName('filterButton');
+//         for (var i = 0; i < filterButtons.length; i++){ filterButtons[i].classList.remove('filterOn'); }
+
+//         // then activate all
+//         filterButton.classList.add('filterOn');
+//         FilterProjects(filterId, true);
+//     }
+//     // activating something other than 'all'
+//     else if (!filterButton.classList.contains('filterOn')) {
+//         // if 'all' is activated, deactivate it and deactivate all projects
+//         if (document.getElementById('all').classList.contains('filterOn')){
+//             document.getElementById('all').classList.remove('filterOn');
+//             FilterProjects('all', false);
+//         }
+
+//         // then activate the filter
+//         filterButton.classList.add('filterOn');
+//         FilterProjects(filterId, true);
+//     }
+//     // deactivate something other than 'all'
+//     else if (filterId != 'all' && filterButton.classList.contains('filterOn')){
+//         filterButton.classList.remove('filterOn');
+//         FilterProjects(filterId, false);
+//     }
+// }
+
+// function FilterProjects(selectedFilter, isOn) {
+//     if (selectedFilter == 'all'){
+//         // get all the projects
+//         const projects = document.getElementsByClassName('projectBox');
+//         // make all visible
+//         if (isOn) { for (var i = 0; i < projects.length; i++){ projects[i].classList.add('filteredOn'); } }
+//         else { for (var i = 0; i < projects.length; i++){ projects[i].classList.remove('filteredOn'); } }
+//     }
+//     else {
+//         // get all the projects with the filter name
+//         const projects = document.getElementsByClassName(selectedFilter);
+//         if (isOn) { for (var i = 0; i < projects.length; i++){ projects[i].classList.add('filteredOn'); } }
+//         else { for (var i = 0; i < projects.length; i++){ projects[i].classList.remove('filteredOn'); } }
+//     }
+// }
 
 
